b4d6aee0103f7747940d2894ba5f1a39c61426eb
rexy siwabessy <rexysiwabessy@gmail.com>
2023-03-25 16:23:55 +0700

    update

---
+++ app/Models/VRegularOrderEntryUploadDetail.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VRegularOrderEntryUploadDetail extends Model
+{
+    use HasFactory;
+    protected $table = 'v_regular_order_entry_upload_detail';
+}
