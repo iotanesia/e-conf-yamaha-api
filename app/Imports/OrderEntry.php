8d1fa87e85dce79f7817e165d0afb8d95e5f1930
ebects <aleekhabib@gmail.com>
2023-04-04 09:39:15 +0700

    fix : import

--- app/Imports/OrderEntry.php
+++ app/Imports/OrderEntry.php
@@ -54,7 +54,7 @@ class OrderEntry implements ToCollection, WithChunkReading, WithStartRow, WithMu
         try {
             $id_regular_order_entry_upload = $this->id_regular_order_entry_upload;
             $ext = [];
-            foreach ($collection->chunk(1000) as $i => $chunk) {
+            foreach ($collection->chunk(10000) as $i => $chunk) {
 
                 $filteredData = collect($chunk)->filter(function ($row){
                     $fillter_yearmonth = $this->params['year'].$this->params['month'];
@@ -62,36 +62,31 @@ class OrderEntry implements ToCollection, WithChunkReading, WithStartRow, WithMu
                     return in_array($row[7],['940E']) && $fillter_yearmonth == $deliver_yearmonth;
                 });
 
-                $filteredData->each(function ($row) use ($id_regular_order_entry_upload) {
-                      $uuid = (String) Str::uuid();
-                      $detail = QueryRegularOrderEntryUploadDetail::created([
-                            'id_regular_order_entry_upload' => $id_regular_order_entry_upload,
-                            'code_consignee' => trim($row[1]),
-                            'model' => trim($row[4]),
-                            'item_no' => trim($row[5]),
-                            'disburse' => trim($row[12]),
-                            'delivery' => trim($row[14]),
-                            'etd_jkt' => trim($row[14]),
-                            'etd_wh' => Carbon::parse(trim($row[14]))->subDays(2)->format('Ymd'),
-                            'etd_ypmi' => Carbon::parse(trim($row[14]))->subDays(4)->format('Ymd'),
-                            'qty' => trim($row[15]),
-                            'status' => trim($row[20]),
-                            'order_no' => trim($row[22]),
-                            'cust_item_no' => trim($row[23]),
-                            'uuid' => $uuid,
-                            'created_at' => now(),
-                            'updated_at' => now(),
-                        ],false);
-
-                        OrderEntryBox::dispatch([
-                            'code_consignee' => trim($row[1]),
-                            'item_no' => trim($row[5]),
-                            'qty' => trim($row[15]),
-                            'uuid_regular_order_entry_upload_detail' => $uuid,
-                            'id_regular_order_entry_upload_detail' => $detail->id,
-                        ]);
+                $data = $filteredData->map(function ($row) use ($id_regular_order_entry_upload) {
+                    return [
+                        'id_regular_order_entry_upload' => $id_regular_order_entry_upload,
+                        'code_consignee' => trim($row[1]),
+                        'model' => trim($row[4]),
+                        'item_no' => trim($row[5]),
+                        'disburse' => trim($row[12]),
+                        'delivery' => trim($row[14]),
+                        'etd_jkt' => trim($row[14]),
+                        'etd_wh' => Carbon::parse(trim($row[14]))->subDays(2)->format('Ymd'),
+                        'etd_ypmi' => Carbon::parse(trim($row[14]))->subDays(4)->format('Ymd'),
+                        'qty' => trim($row[15]),
+                        'status' => trim($row[20]),
+                        'order_no' => trim($row[22]),
+                        'cust_item_no' => trim($row[23]),
+                        'uuid' => (string) Str::uuid(),
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ];
                 });
-                // dd($filteredData);
+
+
+                foreach ($data->toArray() as $params) {
+                    QueryRegularOrderEntryUploadDetail::store($params);
+                }
 
                 // foreach ($chunk as $row) {
 
@@ -171,19 +166,24 @@ class OrderEntry implements ToCollection, WithChunkReading, WithStartRow, WithMu
 
     public function batchSize(): int
     {
-        return 500;
+        return 10000;
     }
 
     public function chunkSize(): int
     {
-        return 1000;
+        return 10000;
     }
 
     public function registerEvents(): array
     {
         return [
             AfterImport::class => function (AfterImport $event){
+                // dd($event);
+
                 QueryRegularOrderEntryUpload::updateStatusAfterImport($this->id_regular_order_entry_upload);
+                OrderEntryBox::dispatch([
+                    'id_regular_order_entry_upload' => $this->id_regular_order_entry_upload
+                ]);
             }
         ];
     }
