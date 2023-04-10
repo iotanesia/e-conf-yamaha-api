98bf2b8efa4de9b0c96b2ed5ef69059e51f15419
alfinorossesa <alfino.zxcvbnm@gmail.com>
2023-04-10 12:04:25 +0700

    update order entry upload

--- app/Query/QueryRegularOrderEntryUpload.php
+++ app/Query/QueryRegularOrderEntryUpload.php
@@ -117,7 +117,7 @@ class QueryRegularOrderEntryUpload extends Model {
 
             $file = $request->file('file');
             // $filename = $file->getClientOriginalName();
-            $filename = 'OE-'.$request->month.$request->year.'-'.$store->datasource.'-batch(0'.$request->iteration.')';
+            $filename = 'OE-'.$request->month.$request->year.'-'.$store->datasource.'-0'.$request->iteration;
             $ext = $file->getClientOriginalExtension();
             if(!in_array($ext,['xlx','xlsx','xlsb'])) throw new \Exception("file format error", 400);
             $savedname = (string) Str::uuid().'.'.$ext;
