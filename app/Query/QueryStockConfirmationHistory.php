dd2b4f849b7f780c8687e162f2bafbd481fcc529
alfinorossesa <alfino.zxcvbnm@gmail.com>
2023-04-12 15:45:22 +0700

    update

--- app/Query/QueryStockConfirmationHistory.php
+++ app/Query/QueryStockConfirmationHistory.php
@@ -352,7 +352,7 @@ class QueryStockConfirmationHistory extends Model {
             $status = $stock_confirmation->status;
             $in_stock_wh = $stock_confirmation->in_wh;
             $in_wh_total = $in_stock_wh + $delivery_plan_box->qty_pcs_box;
-            $in_dc_total = $stock_confirmation->in_dc - $in_wh_total;
+            $in_dc_total = $stock_confirmation->in_dc - $delivery_plan_box->qty_pcs_box;
             $stock_confirmation->in_wh = $in_wh_total;
             $stock_confirmation->in_dc = $in_dc_total;
             $stock_confirmation->status_outstock = $status == Constant::IS_ACTIVE ? 2 : 2;
