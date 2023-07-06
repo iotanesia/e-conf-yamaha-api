41133e3d7622bf04d0a3f55a72c1b7759369dc11
alfinorossesa <alfino.zxcvbnm@gmail.com>
2023-07-06 17:38:18 +0700

    update

--- app/Query/QueryRegularOrderEntryUpload.php
+++ app/Query/QueryRegularOrderEntryUpload.php
@@ -205,7 +205,7 @@ class QueryRegularOrderEntryUpload extends Model {
                 'month' => $request->month,
             ]),storage_path().'/app/'.$params['filepath']);
 
-            sleep(4);
+            sleep(2);
             RegularOrderEntryUploadDetail::where([
                 'id_regular_order_entry_upload' => $store->id
             ])
