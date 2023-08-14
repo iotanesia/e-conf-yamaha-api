0390273e0cf2a8b3837e013ae92d6ee2c75123e6
alfinorossesa <alfino.zxcvbnm@gmail.com>
2023-08-14 21:31:36 +0700

    update mst lsp

--- app/Query/QueryMstLsp.php
+++ app/Query/QueryMstLsp.php
@@ -19,14 +19,26 @@ class QueryMstLsp extends Model {
     {
         $key = self::cast.json_encode($params->query());
         return Helper::storageCache($key, function () use ($params){
-            $query = self::where(function ($query) use ($params){
-               if($params->kueri) $query->where('name',"%$params->kueri%");
-
-            });
-            if($params->withTrashed == 'true') $query->withTrashed();
+            $query = Model::select('mst_lsp.code_consignee',
+                DB::raw("string_agg(DISTINCT mst_lsp.id::character varying, ',') as id_lsp"),
+                DB::raw("string_agg(DISTINCT mst_lsp.name::character varying, ',') as name"),
+                DB::raw("string_agg(DISTINCT a.name::character varying, ',') as type_delivery"),
+                DB::raw("string_agg(DISTINCT b.nick_name::character varying, ',') as cust_name")
+            )->leftJoin('mst_type_delivery as a','mst_lsp.id_type_delivery','a.id')
+            ->leftJoin('mst_consignee as b','mst_lsp.code_consignee','b.code')
+            ->groupBy('mst_lsp.code_consignee');
             $data = $query
-            ->orderBy('id','asc')
+            ->orderBy('id_lsp','asc')
             ->paginate($params->limit ?? null);
+
+            $data->transform(function ($item) {
+                return [
+                    'name' => explode(',', $item->name),
+                    'type_delivery' => explode(',',$item->type_delivery),
+                    'cust_name' => $item->cust_name
+                ];
+            });
+
             return [
                 'items' => $data->items(),
                 'last_page' => $data->lastPage(),
@@ -51,7 +63,14 @@ class QueryMstLsp extends Model {
         try {
 
             $params = $request->all();
-            self::create($params);
+
+            foreach ($params['id_type_delivery'] as $key => $value) {
+                Model::create([
+                    'name' => $params['name'][$key],
+                    'id_type_delivery' => $value,
+                    'code_consignee' => $params['code_consignee']
+                ]);
+            }
 
             if($is_transaction) DB::commit();
             Cache::flush([self::cast]); //delete cache
@@ -74,8 +93,17 @@ class QueryMstLsp extends Model {
             $params = $request->all();
             $update = self::find($params['id']);
             if(!$update) throw new \Exception("id tida ditemukan", 400);
-            $update->fill($params);
-            $update->save();
+
+            $consignee = Model::where('code_consignee', $update->code_consignee)->get();
+            foreach ($consignee as $key => $value) {
+                $upd = Model::find($value->id);
+                $upd->update([
+                    'name' => $params['name'][$key],
+                    'id_type_delivery' => $params['id_type_delivery'][$key],
+                    'code_consignee' => $params['code_consignee']
+                ]);
+            }
+            
             if($is_transaction) DB::commit();
             Cache::flush([self::cast]); //delete cache
         } catch (\Throwable $th) {
