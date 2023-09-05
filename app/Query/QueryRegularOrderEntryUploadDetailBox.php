27c242a50589547fd7f835039e80c313216e470a
alfinorossesa <alfino.zxcvbnm@gmail.com>
2023-09-05 15:33:48 +0700

    update upload dtail box

--- app/Query/QueryRegularOrderEntryUploadDetailBox.php
+++ app/Query/QueryRegularOrderEntryUploadDetailBox.php
@@ -6,7 +6,8 @@ use App\Models\RegularOrderEntryUploadDetailBox AS Model;
 use App\Models\RegularOrderEntryUploadDetail;
 use App\Models\MstBox;
 use BaconQrCode\Common\Mode;
-use DB;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use PhpParser\Node\Expr\AssignOp\Mod;
 
@@ -22,7 +23,7 @@ class QueryRegularOrderEntryUploadDetailBox extends Model {
             ->get();
 
         return [
-            'items' => $data->map(function ($item) use ($id){
+            'items' => $data->map(function ($item) use ($id,$part){
 
                 $p = $item->length == null || $item->length == 0 ? 0 : round($item->length/1000,1);
                 $l = $item->width == null || $item->width == 0 ? 0 : round($item->width/1000,1);
@@ -33,6 +34,12 @@ class QueryRegularOrderEntryUploadDetailBox extends Model {
                 $set["name"] = $item->qty." pcs";
                 $set["size"] = $p." x ".$l." x ".$t;
                 $set["count"] = self::getCountBox($id, $item->id);
+                $set['cust_name'] = $part->refConsignee->nick_name;
+                $set['item_no'] = $part->item_no;
+                $set['item_name'] = $part->refMstPart->description;
+                $set['order_no'] = $part->order_no;
+                $set['qty'] = $part->qty;
+                $set['etd_jkt'] = $part->etd_jkt;
 
                 return $set;
             }),
@@ -60,25 +67,47 @@ class QueryRegularOrderEntryUploadDetailBox extends Model {
 
             if (!count($box['id_box'])) throw new \Exception("Request must be filled", 400);
             $dataOrdered = RegularOrderEntryUploadDetail::find($id) ?? null;
-            $countOrdered = $dataOrdered->qty ?? 0;
-            $uuidOrdered = $dataOrdered->uuid ?? null;
-            $countBox = 0;
-            foreach ($box['id_box'] as $value) {
-                $qtyBox = MstBox::find($value)->qty ?? 0;
-                $countBox = $countBox + $qtyBox;
-            }
-            if($countBox < $countOrdered)
-                throw new \Exception("the total quantity of the box is not sufficient for the total quantity of the order", 400);
+            // $countOrdered = $dataOrdered->qty ?? 0;
+            // $uuidOrdered = $dataOrdered->uuid ?? null;
+            // $countBox = 0;
+            // foreach ($box['id_box'] as $value) {
+            //     $qtyBox = MstBox::find($value)->qty ?? 0;
+            //     $countBox = $countBox + $qtyBox;
+            // }
+            // if($countBox < $countOrdered)
+            //     throw new \Exception("the total quantity of the box is not sufficient for the total quantity of the order", 400);
             Model::where('id_regular_order_entry_upload_detail',$id)->forceDelete();
-            foreach ($box['id_box'] as $value){
-                $data["id_box"] = $value;
-                $data["uuid"] = Str::uuid();
-                $data["uuid_regular_order_entry_upload_detail"] = $uuidOrdered;
-                $data["id_regular_order_entry_upload_detail"] = $id;
-                $data["created_at"] = date('Y-m-d H:i:s');
-                $data["updated_at"] = date('Y-m-d H:i:s');
-                Model::insert($data);
-            }
+            $count = count($params->qty);
+            // foreach ($box['id_box'] as $value){
+                for ($i=0; $i < $count; $i++) { 
+                    $updateDetail = RegularOrderEntryUploadDetail::create([
+                        "id_regular_order_entry_upload" => $dataOrdered->id_regular_order_entry_upload,
+                        "code_consignee" => $dataOrdered->code_consignee,
+                        "model" => $dataOrdered->model,
+                        "item_no" => $dataOrdered->item_no,
+                        "disburse" => $dataOrdered->disburse,
+                        "delivery" => $dataOrdered->delivery,
+                        "qty" => $params->qty[$i],
+                        "status" => $dataOrdered->status,
+                        "etd_ypmi" => Carbon::parse($params->etd_jkt[$i])->subDays(4)->format('Y-m-d'),
+                        "etd_wh" => Carbon::parse($params->etd_jkt[$i])->subDays(2)->format('Y-m-d'),
+                        "etd_jkt" => $params->etd_jkt[$i],
+                        "order_no" => $dataOrdered->order_no,
+                        "cust_item_no" => $dataOrdered->cust_item_no,
+                        "uuid" => $dataOrdered->uuid,
+                        "jenis" => $dataOrdered->jenis,
+                    ]);
+    
+                    $data["id_box"] = $params->id_box[$i];
+                    $data["uuid"] = Str::uuid();
+                    $data["uuid_regular_order_entry_upload_detail"] = $dataOrdered->uuid;
+                    $data["id_regular_order_entry_upload_detail"] = $updateDetail->id;
+                    $data["created_at"] = date('Y-m-d H:i:s');
+                    $data["updated_at"] = date('Y-m-d H:i:s');
+                    Model::insert($data);
+                }
+            // }
+            $dataOrdered->delete();
             if($is_transaction)DB::commit();
         }catch (\Throwable $th) {
             if($is_transaction) DB::rollBack();
