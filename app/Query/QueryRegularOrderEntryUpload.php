03fb85cb4a640dfad58f07e1e630788d27544f00
alfinorossesa <alfino.zxcvbnm@gmail.com>
2023-10-30 16:41:08 +0700

    update set

--- app/Query/QueryRegularOrderEntryUpload.php
+++ app/Query/QueryRegularOrderEntryUpload.php
@@ -613,16 +613,12 @@ class QueryRegularOrderEntryUpload extends Model {
 
             $upload_detail_set = RegularOrderEntryUploadDetailSet::where('id_regular_order_entry', $upload->id_regular_order_entry)->get()->toArray();
             foreach ($upload_detail_set as $key => $value) {
-
-                $check_set = RegularDeliveryPlanSet::where('id_delivery_plan', $value['id_detail'])->get();
-                if (count($check_set) == 0) {
-                    RegularDeliveryPlanSet::create([
-                        "id_delivery_plan" => $value['id_detail'],
-                        "item_no" => $value['item_no'],
-                        "id_regular_order_entry" => $value['id_regular_order_entry'],
-                        "qty" => $value['qty'],
-                    ]);
-                }
+                RegularDeliveryPlanSet::create([
+                    "id_delivery_plan" => $value['id_detail'],
+                    "item_no" => $value['item_no'],
+                    "id_regular_order_entry" => $value['id_regular_order_entry'],
+                    "qty" => $value['qty'],
+                ]);
             }
 
             $deliv_plan_set = RegularDeliveryPlanSet::select('regular_delivery_plan_set.id_delivery_plan',
