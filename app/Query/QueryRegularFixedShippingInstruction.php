f53403812ec2338f960e97b4238d0c6887d6076d
alfinorossesa <alfino.zxcvbnm@gmail.com>
2024-01-11 16:44:47 +0700

    update net weight

--- app/Query/QueryRegularFixedShippingInstruction.php
+++ app/Query/QueryRegularFixedShippingInstruction.php
@@ -165,10 +165,10 @@ class QueryRegularFixedShippingInstruction extends Model {
                 foreach ($box as $box_item){
                     if ($box_item->refRegularDeliveryPlan->item_no == null) {
                         foreach ($box_item->refRegularDeliveryPlan->manyDeliveryPlanSet as $set) {
-                            $total_net_weight += ((($set->refBox->unit_weight_gr * (array_sum($box->pluck('qty_pcs_box')->toArray()) / count($box_item->refRegularDeliveryPlan->manyDeliveryPlanSet)))/1000) * $item->summary_box) / count($box);
-                            $total_gross_weight += (((($set->refBox->unit_weight_gr * (array_sum($box->pluck('qty_pcs_box')->toArray()) / count($box_item->refRegularDeliveryPlan->manyDeliveryPlanSet)))/1000) + $set->refBox->outer_carton_weight) * $item->summary_box) / count($box);
-                            $count_meas += (($set->refBox->length * $set->refBox->width * $set->refBox->height) / 1000000000) / count($box);
+                            $total_net_weight += ((($set->refBox->unit_weight_gr * ((array_sum($box->pluck('qty_pcs_box')->toArray()) / count($box) / count($box_item->refRegularDeliveryPlan->manyDeliveryPlanSet)))/1000)));
+                            $total_gross_weight += (((($set->refBox->unit_weight_gr * ((array_sum($box->pluck('qty_pcs_box')->toArray()) / count($box)) / count($box_item->refRegularDeliveryPlan->manyDeliveryPlanSet)))/1000) + $set->refBox->outer_carton_weight));
                         } 
+                        $count_meas += ($box_item->refMstBox->length * $box_item->refMstBox->width * $box_item->refMstBox->height) / 1000000000;
                     } else {
                         $count_net_weight = $box_item->refMstBox->unit_weight_gr;
                         $count_outer_carton_weight = $box_item->refMstBox->outer_carton_weight;
