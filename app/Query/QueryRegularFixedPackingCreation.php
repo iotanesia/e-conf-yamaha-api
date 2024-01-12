b923a64d1d2a08c5bf36026b4441493ab433cffe
alfinorossesa <alfino.zxcvbnm@gmail.com>
2024-01-12 11:04:04 +0700

    update part

--- app/Query/QueryRegularFixedPackingCreation.php
+++ app/Query/QueryRegularFixedPackingCreation.php
@@ -170,6 +170,7 @@ class QueryRegularFixedPackingCreation extends Model {
             DB::raw("string_agg(DISTINCT regular_fixed_quantity_confirmation.item_no::character varying, ',') as item_no"),
             DB::raw("string_agg(DISTINCT regular_fixed_quantity_confirmation.order_no::character varying, ',') as order_no"),
             DB::raw('MAX(regular_fixed_quantity_confirmation.in_wh) as in_wh'),
+            DB::raw('count(regular_fixed_quantity_confirmation.id) as count'),
             )
             ->where('id_fixed_actual_container', $id)
             ->groupBy('id_regular_delivery_plan')
@@ -187,6 +188,7 @@ class QueryRegularFixedPackingCreation extends Model {
                 $item->item_name = $item->refRegularDeliveryPlan->item_no == null ? $mst_part->toArray() : trim($item->refRegularDeliveryPlan->refPart->description);
                 $item->cust_name = $item->refRegularDeliveryPlan->refConsignee->nick_name;
                 $item->no_invoice = $item->refFixedActualContainer->no_packaging;
+                $item->in_wh = count(explode(',', $item->count)) . ' x ' . $item->in_wh;
                 unset(
                     $item->refRegularDeliveryPlan,
                     $item->refFixedActualContainer
