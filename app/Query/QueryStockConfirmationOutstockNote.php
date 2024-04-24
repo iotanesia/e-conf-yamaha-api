e0fb351e321917bb31d5ee9a726a6f7d5bd04007
alfinorossesa <alfino.zxcvbnm@gmail.com>
2024-04-24 16:20:25 +0700

    update save outstock

--- app/Query/QueryStockConfirmationOutstockNote.php
+++ app/Query/QueryStockConfirmationOutstockNote.php
@@ -176,10 +176,9 @@ class QueryStockConfirmationOutstockNote extends Model {
                         $fixed_quantity_confirmation->save();
         
                         foreach ($fixed_quantity_confirmation->refRegularDeliveryPlan->manyDeliveryPlanBox as $item_box) {
-                            $check_outstock = RegularStokConfirmationHistory::where('id_regular_delivery_plan_box', $item_box->id)->where('type', 'OUTSTOCK')->first();
                             $qr_key = $request->id[0];
                             $check_status_outstock = RegularStokConfirmationTemp::where('qr_key', $qr_key)->where('status_outstock', 3)->first();
-                            if ($check_outstock && $check_status_outstock) {
+                            if ($check_status_outstock) {
                                 $fixed_quantity_confirmation_box = RegularFixedQuantityConfirmationBox::where('id_regular_delivery_plan_box', $item_box->id)->first();
                                 if(!$fixed_quantity_confirmation_box) {
                                     $fixed_quantity_confirmation_box = new RegularFixedQuantityConfirmationBox;
