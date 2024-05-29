8501063c1bc8c8155f647893b020f1042131789c
alfinorossesa <alfino.zxcvbnm@gmail.com>
2024-05-29 13:28:56 +0700

    update casemarks

--- resources/views/pdf/casemarks/casemarks_doc.blade.php
+++ resources/views/pdf/casemarks/casemarks_doc.blade.php
@@ -75,7 +75,11 @@
                         <td class="text-center" style="font-size: 40px; font-weight: 500; vertical-align=top; padding: 20px;">
                             <p style="margin:0 0 15px 0; padding:0;"><b>YAMAHA</b></p>
                             <p style="margin:0 0 15px 0; padding:0;"><b>{{ $order ?? null }}</b></p>
-                            <p style="margin:0 0 15px 0; padding:0;"><b>{{ $item->manyFixedQuantityConfirmation()->where('order_no', $order)->first()->cust_item_no ?? null }}</b></p>
+                            @if ($item->datasource == 'YPMJ')
+                                <p style="margin:0 0 15px 0; padding:0;"><b>YMBP</b></p>
+                            @else
+                                <p style="margin:0 0 15px 0; padding:0;"><b>{{ $item->manyFixedQuantityConfirmation()->where('order_no', $order)->first()->cust_item_no ?? null }}</b></p>
+                            @endif
                             <p style="margin:0 0 15px 0; padding:0;"><b>{{ $item->refPartOfDischarge()->where('id_mot', $item->id_mot)->first()->port ?? null }}</b></p>
                             <p style="margin:0 0 15px 0; padding:0;"><b>MADE IN INDONESIA</b></p>
                             <p style="margin:0 0 15px 0; padding:0;"><b>INV.No. {{ $item->no_packaging }}</b></p>
