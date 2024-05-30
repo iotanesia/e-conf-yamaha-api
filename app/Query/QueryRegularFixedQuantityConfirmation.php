120930ea6b60421faa96df991100f29f00470378
alfinorossesa <alfino.zxcvbnm@gmail.com>
2024-05-30 10:36:46 +0700

    update casemarks

--- app/Query/QueryRegularFixedQuantityConfirmation.php
+++ app/Query/QueryRegularFixedQuantityConfirmation.php
@@ -1473,7 +1473,7 @@ class QueryRegularFixedQuantityConfirmation extends Model {
 
     public static function printCasemarks($request,$id,$pathToFile,$filename)
     {
-        try {
+        // try {
             $data = RegularFixedActualContainer::where('id', $id)->get();
             $id_delivery_plan = [];
             foreach ($data[0]->manyFixedQuantityConfirmation as $id_delivery) {
@@ -1485,14 +1485,39 @@ class QueryRegularFixedQuantityConfirmation extends Model {
             $res_box_set = [];
             foreach ($deliv_plan as $key => $deliv_value) {
                 if ($deliv_value->item_no !== null) {
-                    $res = $deliv_value->manyFixedQuantityConfirmationBox->map(function($item) use($id) {
+                    $res = $deliv_value->manyFixedQuantityConfirmationBox->map(function($item, $i) use($id, $deliv_value, $deliv_plan, $key) {
                         if ($item->refFixedQuantityConfirmation->id_fixed_actual_container == $id) {
                             $res['qrcode'] = $item->qrcode;
                             $res['item_no'] = [$item->refRegularDeliveryPlan->item_no];
-                            $res['qty_pcs_box'] = [$item->qty_pcs_box];
                             $res['item_no_series'] = [$item->refMstBox->item_no_series];
                             $res['unit_weight_kg'] = [($item->refMstBox->unit_weight_gr * $item->qty_pcs_box)/1000];
-                            $res['total_gross_weight'] = [(($item->refMstBox->unit_weight_gr * $item->qty_pcs_box)/1000) + $item->refMstBox->outer_carton_weight];
+                            if ($item->refRegularDeliveryPlan->datasource == 'YPMJ') {
+                                if ($key+1 == count($deliv_plan)) {
+                                    if ($i+1 == count($deliv_value->manyFixedQuantityConfirmationBox)) {
+                                        $res['total_gross_weight'] = [(($item->refMstBox->unit_weight_gr * $item->refRegularDeliveryPlan->qty)/1000) + (ceil($item->refRegularDeliveryPlan->qty / $item->refMstBox->qty) * $item->refMstBox->weight_inner_carton) + $item->refRegularDeliveryPlan->refOuterType->outer_weight];
+                                        $res['meas_ypmj'] = $item->refRegularDeliveryPlan->refOuterType->measurement;
+                                        $res['qty_pcs_box'] = [$item->refRegularDeliveryPlan->qty];
+                                    } else {
+                                        $res['total_gross_weight'] = [0];
+                                        $res['meas_ypmj'] = 0;
+                                        $res['qty_pcs_box'] = [0];
+                                    }
+                                } else {
+                                    if ($i+1 == count($deliv_value->manyFixedQuantityConfirmationBox)) {
+                                        $res['total_gross_weight'] = [(($item->refMstBox->unit_weight_gr * $item->refRegularDeliveryPlan->qty)/1000) + (ceil($item->refRegularDeliveryPlan->qty / $item->refMstBox->qty) * $item->refMstBox->weight_inner_carton)];
+                                        $res['meas_ypmj'] = 0;
+                                        $res['qty_pcs_box'] = [$item->refRegularDeliveryPlan->qty];
+                                    } else {
+                                        $res['total_gross_weight'] = [0];
+                                        $res['meas_ypmj'] = 0;
+                                        $res['qty_pcs_box'] = [0];
+                                    }
+                                }
+                            } else {
+                                $res['total_gross_weight'] = [(($item->refMstBox->unit_weight_gr * $item->qty_pcs_box)/1000) + $item->refMstBox->outer_carton_weight];
+                                $res['meas_ypmj'] = [0];
+                                $res['qty_pcs_box'] = [$item->qty_pcs_box];
+                            }
                             $res['length'] = $item->refMstBox->length;
                             $res['width'] = $item->refMstBox->width;
                             $res['height'] = $item->refMstBox->height;
@@ -1647,19 +1672,20 @@ class QueryRegularFixedQuantityConfirmation extends Model {
                     $count_data[] = 'count';
                 }
             }
-            
+            // dd(self::groupByQRCode($box));
             Pdf::loadView('pdf.casemarks.casemarks_doc',[
                 'count_data' => count($count_data),
                 'data' => $data,
-                'box' => $boxArray
+                'box' => $boxArray,
+                'boxYPMJ' => self::groupByQRCode($box)
             ])
             ->save($pathToFile)
             ->setPaper('A4','potrait')
             ->download($filename);
 
-        } catch (\Throwable $th) {
-            return Helper::setErrorResponse($th);
-        }
+        // } catch (\Throwable $th) {
+        //     return Helper::setErrorResponse($th);
+        // }
     }
 
     public static function printPackaging($request,$id,$pathToFile,$filename)
--- resources/views/pdf/casemarks/casemarks_doc.blade.php
+++ resources/views/pdf/casemarks/casemarks_doc.blade.php
@@ -63,56 +63,124 @@
     @foreach ($data as $key => $item) 
     {{-- @if ($check == null) part set --}}
     @foreach (array_unique($item->manyFixedQuantityConfirmation->pluck('order_no')->toArray()) as $key => $order)
-        @foreach ($box[$order] as $jml => $box_item)
-            @for ($i=1; $i<=2; $i++)
+        @if ($item->datasource == "YPMJ")
+            @foreach ($boxYPMJ as $jml => $box_item)
+                @for ($i=1; $i<=2; $i++)
 
-            @if ($i == 1)
-                <table style="margin-bottom: 45px;">
-            @else
-                <table>
-            @endif
-                    <tr>
-                        <td class="text-center" style="font-size: 40px; font-weight: 500; vertical-align=top; padding: 20px;">
-                            <p style="margin:0 0 15px 0; padding:0;"><b>YAMAHA</b></p>
-                            <p style="margin:0 0 15px 0; padding:0;"><b>{{ $order ?? null }}</b></p>
-                            @if ($item->datasource == 'YPMJ')
-                                <p style="margin:0 0 15px 0; padding:0;"><b>YMBP</b></p>
-                            @else
+                @if ($i == 1)
+                    <table style="margin-bottom: 45px;">
+                @else
+                    <table>
+                @endif
+                        <tr>
+                            <td class="text-center" style="font-size: 40px; font-weight: 500; vertical-align=top; padding: 20px;">
+                                <p style="margin:0 0 15px 0; padding:0;"><b>YAMAHA</b></p>
+                                <p style="margin:0 0 15px 0; padding:0;"><b>{{ $order ?? null }}</b></p>
+                                <p style="margin:0 0 15px 0; padding:0;"><b>-</b></p>
+                                <p style="margin:0 0 15px 0; padding:0;"><b>{{ $item->refPartOfDischarge()->where('id_mot', $item->id_mot)->first()->port ?? null }}</b></p>
+                                <p style="margin:0 0 15px 0; padding:0;"><b>MADE IN INDONESIA</b></p>
+                                <p style="margin:0 0 15px 0; padding:0;"><b>INV.No. {{ $item->no_packaging }}</b></p>
+                                <p style="margin:0 0 15px 0; padding:0;"><b>C/No. : {{ $key > 0 ? $key+1 : $jml+1 }}</b></p>
+                            </td>
+                            <td class="text-center" style="font-size: 25px; font-weight: 500; vertical-align=top;">
+                                <p style="padding:0; margin: 0 0 10px 0;"><b>CUSTOMER :</b></p>
+                                <b>{{ $item->refConsignee->nick_name ?? $item->code_consignee }}</b>
+                                <hr>
+                                <table>
+                                    <tr>
+                                        <td class="no-bl no-bt no-bb text-center" rowspan="2"><p style="padding:0; margin:0 0 20px 0;"><b>PART NO.</b></p> </td>
+                                        <td class="no-br no-bt text-center"><p style="padding:0; margin:0;"><b>QTY</b></p> </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no-br no-bb text-center"><p style="padding:0; margin:0;"><b>PCS</b></p> </td>
+                                    </tr>
+                                </table>
+                                <hr>
+                                <table>
+                                    <tr>
+                                        <td class="no-bo">
+                                            <table>
+                                                @foreach ($box_item['item_no'] as $item_no)
+                                                    <tr>
+                                                        <td class="no-br no-bl no-bb no-bt text-center"><b>{{ $item_no }}</b></td>
+                                                    </tr>
+                                                @endforeach
+                                            </table>
+                                        </td>
+                                        <td class="no-bo">
+                                            <table>
+                                                @foreach ($box_item['qty_pcs_box'] as $qty_pcs_box)
+                                                    @if ($qty_pcs_box !== 0)
+                                                        <tr>
+                                                            <td class="no-br no-bb no-bt text-center"><b>{{ $qty_pcs_box }}</b></td>
+                                                        </tr>
+                                                    @endif
+                                                @endforeach
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </table>
+                                <hr>
+                                <table>
+                                    <tr>
+                                        <td class="no-bl no-br no-bt no-bb text-center"><p style="padding:0; margin:0 0 20px 0;"><b>GW :</b></p> </td>
+                                        <td class="no-br no-bt no-bb text-center"><p style="padding:0; margin:0 0 20px 0;"><b>{{ array_sum($box_item['total_gross_weight']) }}</b></p> </td>
+                                        <td class="no-br no-bt no-bb text-center"><p style="padding:0; margin:0 0 20px 0;"><b>KG</b></p> </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                @endfor
+            @endforeach
+        @else
+            @foreach ($box[$order] as $jml => $box_item)
+                @for ($i=1; $i<=2; $i++)
+
+                @if ($i == 1)
+                    <table style="margin-bottom: 45px;">
+                @else
+                    <table>
+                @endif
+                        <tr>
+                            <td class="text-center" style="font-size: 40px; font-weight: 500; vertical-align=top; padding: 20px;">
+                                <p style="margin:0 0 15px 0; padding:0;"><b>YAMAHA</b></p>
+                                <p style="margin:0 0 15px 0; padding:0;"><b>{{ $order ?? null }}</b></p>
                                 <p style="margin:0 0 15px 0; padding:0;"><b>{{ $item->manyFixedQuantityConfirmation()->where('order_no', $order)->first()->cust_item_no ?? null }}</b></p>
-                            @endif
-                            <p style="margin:0 0 15px 0; padding:0;"><b>{{ $item->refPartOfDischarge()->where('id_mot', $item->id_mot)->first()->port ?? null }}</b></p>
-                            <p style="margin:0 0 15px 0; padding:0;"><b>MADE IN INDONESIA</b></p>
-                            <p style="margin:0 0 15px 0; padding:0;"><b>INV.No. {{ $item->no_packaging }}</b></p>
-                            <p style="margin:0 0 15px 0; padding:0;"><b>C/No. : {{ $key > 0 ? $key+1 : $jml+1 }}</b></p>
-                        </td>
-                        <td class="text-center" style="font-size: 25px; font-weight: 500; vertical-align=top;">
-                            <p style="padding:0; margin: 0 0 10px 0;"><b>CUSTOMER :</b></p>
-                            <b>{{ $item->refConsignee->nick_name ?? $item->code_consignee }}</b>
-                            <hr>
-                            <p style="padding:0; margin: 0 0 10px 0;"><b>PART NO.</b></p>
-                            @foreach ($box_item['item_no_series'] as $item_no_series)
-                            <b>{{ substr($item_no_series,0,-3) }}</b> <br>
-                            @endforeach
-                            <hr>
-                            <table>
-                                <tr>
-                                    <td class="no-bl no-bt no-bb text-center"><p style="padding:0; margin:0 0 20px 0;"><b>QTY</b></p> </td>
-                                    <td class="no-br no-bt no-bb text-center"><p style="padding:0; margin:0 0 20px 0;"><b>GW</b></p> </td>
-                                </tr>
-                                <tr>
-                                    <td class="no-bl text-center"><b>{{ $box_item['qty_pcs_box'][0] ?? null }}</b></td>
-                                    <td class="no-br text-center"><b>{{ round(array_sum($box_item['total_gross_weight']),2) }}</b></td>
-                                </tr>
-                                <tr>
-                                    <td class="no-bl no-bb text-center"><p style="padding:0; margin:15px 0 0 0;"><b>{{ count($box_item['qty_pcs_box']) > 1 ? 'SET' : 'PCS' }}</b></p></td>
-                                    <td class="no-br no-bb text-center"><p style="padding:0; margin:15px 0 0 0;"><b>KG</b></p></td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                </table>
-            @endfor
-        @endforeach
+                                <p style="margin:0 0 15px 0; padding:0;"><b>{{ $item->refPartOfDischarge()->where('id_mot', $item->id_mot)->first()->port ?? null }}</b></p>
+                                <p style="margin:0 0 15px 0; padding:0;"><b>MADE IN INDONESIA</b></p>
+                                <p style="margin:0 0 15px 0; padding:0;"><b>INV.No. {{ $item->no_packaging }}</b></p>
+                                <p style="margin:0 0 15px 0; padding:0;"><b>C/No. : {{ $key > 0 ? $key+1 : $jml+1 }}</b></p>
+                            </td>
+                            <td class="text-center" style="font-size: 25px; font-weight: 500; vertical-align=top;">
+                                <p style="padding:0; margin: 0 0 10px 0;"><b>CUSTOMER :</b></p>
+                                <b>{{ $item->refConsignee->nick_name ?? $item->code_consignee }}</b>
+                                <hr>
+                                <p style="padding:0; margin: 0 0 10px 0;"><b>PART NO.</b></p>
+                                @foreach ($box_item['item_no_series'] as $item_no_series)
+                                <b>{{ substr($item_no_series,0,-3) }}</b> <br>
+                                @endforeach
+                                <hr>
+                                <table>
+                                    <tr>
+                                        <td class="no-bl no-bt no-bb text-center"><p style="padding:0; margin:0 0 20px 0;"><b>QTY</b></p> </td>
+                                        <td class="no-br no-bt no-bb text-center"><p style="padding:0; margin:0 0 20px 0;"><b>GW</b></p> </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no-bl text-center"><b>{{ $box_item['qty_pcs_box'][0] ?? null }}</b></td>
+                                        <td class="no-br text-center"><b>{{ round(array_sum($box_item['total_gross_weight']),2) }}</b></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="no-bl no-bb text-center"><p style="padding:0; margin:15px 0 0 0;"><b>{{ count($box_item['qty_pcs_box']) > 1 ? 'SET' : 'PCS' }}</b></p></td>
+                                        <td class="no-br no-bb text-center"><p style="padding:0; margin:15px 0 0 0;"><b>KG</b></p></td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                @endfor
+            @endforeach
+        @endif
     @endforeach
     {{-- @elseif ($item->refConsignee->nick_name == 'YMC') YMC
         @foreach ($box as $jml => $box_item)
