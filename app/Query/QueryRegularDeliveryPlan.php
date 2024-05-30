28d9b140eef09b5458f725db547033aa4eea32a9
alfinorossesa <alfino.zxcvbnm@gmail.com>
2024-05-31 01:28:25 +0700

    update case number

--- app/Query/QueryRegularDeliveryPlan.php
+++ app/Query/QueryRegularDeliveryPlan.php
@@ -674,8 +674,7 @@ class QueryRegularDeliveryPlan extends Model {
                         'is_produksi' => 1, 
                         "bucket_produksi" => $current_bucket+1,
                         "period" => $period,
-                        // "case_number" => $case_number
-                        "case_number" => 1
+                        "case_number" => $case_number
                     ]);
                 }
 
