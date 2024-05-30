ce1445ac5eb90ee28ec453e5b549711d4bd97b4c
alfinorossesa <alfino.zxcvbnm@gmail.com>
2024-05-30 11:55:34 +0700

    update casemarks SI

--- app/Query/QueryRegularFixedShippingInstruction.php
+++ app/Query/QueryRegularFixedShippingInstruction.php
@@ -1375,14 +1375,44 @@ class QueryRegularFixedShippingInstruction extends Model {
             $id_fixed_actual = $data[0]->id;
             foreach ($deliv_plan as $key => $deliv_value) {
                 if ($deliv_value->item_no !== null) {
-                    $res = $deliv_value->manyFixedQuantityConfirmationBox->map(function($item) use($id_fixed_actual) {
+                    $res = $deliv_value->manyFixedQuantityConfirmationBox->map(function($item, $i) use($id_fixed_actual, $deliv_value, $deliv_plan, $key) {
                         if ($item->refFixedQuantityConfirmation->id_fixed_actual_container == $id_fixed_actual) {
                             $res['qrcode'] = $item->qrcode;
                             $res['item_no'] = [$item->refRegularDeliveryPlan->item_no];
-                            $res['qty_pcs_box'] = [$item->qty_pcs_box];
                             $res['item_no_series'] = [$item->refMstBox->item_no_series];
                             $res['unit_weight_kg'] = [($item->refMstBox->unit_weight_gr * $item->qty_pcs_box)/1000];
-                            $res['total_gross_weight'] = [(($item->refMstBox->unit_weight_gr * $item->qty_pcs_box)/1000) + $item->refMstBox->outer_carton_weight];
+                            if ($item->refRegularDeliveryPlan->datasource == 'YPMJ') {
+                                if ($key+1 == count($deliv_plan)) {
+                                    if ($i+1 == count($deliv_value->manyFixedQuantityConfirmationBox)) {
+                                        $res['total_gross_weight'] = [(($item->refMstBox->unit_weight_gr * $item->refRegularDeliveryPlan->qty)/1000) + (ceil($item->refRegularDeliveryPlan->qty / $item->refMstBox->qty) * $item->refMstBox->weight_inner_carton) + $item->refRegularDeliveryPlan->refOuterType->outer_weight];
+                                        $res['meas_ypmj'] = $item->refRegularDeliveryPlan->refOuterType->measurement;
+                                        $res['qty_pcs_box'] = [$item->refRegularDeliveryPlan->qty];
+                                        $res['item_no_series_ypmj'] = $item->refMstBox->item_no_series;
+                                    } else {
+                                        $res['total_gross_weight'] = [0];
+                                        $res['meas_ypmj'] = 0;
+                                        $res['qty_pcs_box'] = [0];
+                                        $res['item_no_series_ypmj'] = null;
+                                    }
+                                } else {
+                                    if ($i+1 == count($deliv_value->manyFixedQuantityConfirmationBox)) {
+                                        $res['total_gross_weight'] = [(($item->refMstBox->unit_weight_gr * $item->refRegularDeliveryPlan->qty)/1000) + (ceil($item->refRegularDeliveryPlan->qty / $item->refMstBox->qty) * $item->refMstBox->weight_inner_carton)];
+                                        $res['meas_ypmj'] = 0;
+                                        $res['qty_pcs_box'] = [$item->refRegularDeliveryPlan->qty];
+                                        $res['item_no_series_ypmj'] = $item->refMstBox->item_no_series;
+                                    } else {
+                                        $res['total_gross_weight'] = [0];
+                                        $res['meas_ypmj'] = 0;
+                                        $res['qty_pcs_box'] = [0];
+                                        $res['item_no_series_ypmj'] = null;
+                                    }
+                                }
+                            } else {
+                                $res['total_gross_weight'] = [(($item->refMstBox->unit_weight_gr * $item->qty_pcs_box)/1000) + $item->refMstBox->outer_carton_weight];
+                                $res['meas_ypmj'] = [0];
+                                $res['qty_pcs_box'] = [$item->qty_pcs_box];
+                                $res['item_no_series_ypmj'] = null;
+                            }
                             $res['length'] = $item->refMstBox->length;
                             $res['width'] = $item->refMstBox->width;
                             $res['height'] = $item->refMstBox->height;
@@ -1517,7 +1547,7 @@ class QueryRegularFixedShippingInstruction extends Model {
                 }
                 $boxArray[$order_no][] = $box_item;
             }
-
+            
             $count_qty = 0;
             $count_net_weight = 0;
             $count_gross_weight = 0;
@@ -1541,7 +1571,8 @@ class QueryRegularFixedShippingInstruction extends Model {
             Pdf::loadView('pdf.casemarks.casemarks_doc',[
                 'count_data' => count($count_data),
                 'data' => $data,
-                'box' => $boxArray
+                'box' => $boxArray,
+                'boxYPMJ' => QueryRegularFixedQuantityConfirmation::groupByQRCode($box)
             ])
             ->save($pathToFile)
             ->setPaper('A4','potrait')
