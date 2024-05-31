d56a566480adc2f5a9c4db46df68fa28cc2ea559
alfinorossesa <alfino.zxcvbnm@gmail.com>
2024-05-31 16:29:10 +0700

    update do pakcging

--- app/Query/QueryRegularFixedQuantityConfirmation.php
+++ app/Query/QueryRegularFixedQuantityConfirmation.php
@@ -1907,7 +1907,9 @@ class QueryRegularFixedQuantityConfirmation extends Model {
                 $count_meas_per_order = [];
                 $count_meas_ypmj_per_order = [];
                 $order_no = '';
+                $countItemNo = 0;
                 foreach ($itemSum as $val) {
+                    $countItemNo += count($val['item_no_series']);
                     $count_qty_per_order += array_sum($val['qty_pcs_box']);
                     $count_net_weight_per_order += array_sum($val['unit_weight_kg']);
                     $count_gross_weight_per_order += array_sum($val['total_gross_weight']);
@@ -1920,7 +1922,8 @@ class QueryRegularFixedQuantityConfirmation extends Model {
                     'nett_weight' => $count_net_weight_per_order,
                     'gross_weight' => $count_gross_weight_per_order,
                     'meas' => array_sum($count_meas_per_order),
-                    'meas_ypmj' => $count_meas_ypmj_per_order
+                    'meas_ypmj' => $count_meas_ypmj_per_order,
+                    'count_item_no' => $countItemNo
                 ];
             }
 
--- resources/views/pdf/packaging/packaging_doc.blade.php
+++ resources/views/pdf/packaging/packaging_doc.blade.php
@@ -201,11 +201,7 @@
                             <tr>
                                 @if ($key == 0)
                                     @if ($i == 0)
-                                        @if (count($box_item['item_no_series']) > 1 && count($box[$order]) > 1)
-                                            <td class="text-center" rowspan="{{ count($box[$order]) + count($box_item['item_no_series']) }}">
-                                        @else
-                                            <td class="text-center" rowspan="{{ count($box[$order]) * count($box_item['item_no_series']) }}">
-                                        @endif
+                                        <td class="text-center" rowspan="{{ $sum_res_per_order[$order]['count_item_no'] }}">
                                             YAMAHA <br>
                                             {{ $order ?? null }}  <br>
                                             {{ $item->manyFixedQuantityConfirmation()->where('order_no', $order)->first()->cust_item_no ?? null }} <br>
