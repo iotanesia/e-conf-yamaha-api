d750032125ba86eba7399d74b5a7080180347a8e
alfinorossesa <alfino.zxcvbnm@gmail.com>
2024-05-31 14:06:56 +0700

    udpate generate box

--- app/Query/QueryRegularDeliveryPlan.php
+++ app/Query/QueryRegularDeliveryPlan.php
@@ -670,16 +670,13 @@ class QueryRegularDeliveryPlan extends Model {
                     $case_number_2nd_week++;
                 }
 
-                if(!$is_regenerate_box){
+                if($is_regenerate_box){
                     $delivery_plan->update([
+                        'is_produksi' => 1, 
                         "bucket_produksi" => $current_bucket+1,
                         "period" => $period,
                         // "case_number" => $case_number
                         "case_number" => 1
-                    ]);
-                } else {
-                    $delivery_plan->update([
-                        'is_produksi' => 1, 
                     ]);
                 }
 
