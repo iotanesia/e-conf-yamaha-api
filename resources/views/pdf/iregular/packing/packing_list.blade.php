2482c90a9fb34b9daa57f383bd14216af7552cdd
alfinorossesa <alfino.zxcvbnm@gmail.com>
2024-07-08 17:14:53 +0700

    update dok packaging

--- resources/views/pdf/iregular/packing/packing_list.blade.php
+++ resources/views/pdf/iregular/packing/packing_list.blade.php
@@ -185,9 +185,9 @@
             
         <p class="flying-text text-center">
             YAMAHA <br>
-            {{ $delivery_plan->refOrderEntry->manyOrderEntryPart[0]['order_no'] }}<br>
-            {{ $casemark_data[0]['model_code'] }} <br>
-            {{ $casemark_data[0]['destination'] }} <br>
+            {{ $packing_data->refDeliveryPlan->refOrderEntry->manyOrderEntryPart[0]['order_no'] ?? null }}<br>
+            {{ $casemark_data[0]['model_code'] ?? null }} <br>
+            {{ $casemark_data[0]['destination'] ?? null }} <br>
             MADE IN INDONESIA <br>
             INV. No. {{ $invoice_data->invoice_no }} <br>
             C/No. : 1 - {{ count($data) }}
