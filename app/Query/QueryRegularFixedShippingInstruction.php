84e18bfb3c25e82cec4139e8895be62eca33bdc0
alfinorossesa <alfino.zxcvbnm@gmail.com>
2024-07-30 16:29:58 +0700

    update dok dhipping

--- app/Query/QueryRegularFixedShippingInstruction.php
+++ app/Query/QueryRegularFixedShippingInstruction.php
@@ -501,10 +501,10 @@ class QueryRegularFixedShippingInstruction extends Model {
     {
         try {
             $data = RegularFixedShippingInstructionCreation::where('id_fixed_shipping_instruction', $id)->first();
-            $data->instruction_date = Carbon::parse($data->instruction_date)->subDay(2)->format('D, M d, Y');
-            $data->etd_wh = Carbon::parse($data->etd_jkt)->subDay(2)->format('D, M d, Y');
-            $data->eta_destination = Carbon::parse($data->eta_destination)->subDay(2)->format('M d, Y');
-            $data->etd_jkt = Carbon::parse($data->etd_jkt)->subDay(2)->format('M d, Y');
+            // $data->instruction_date = Carbon::parse($data->instruction_date)->subDay(2)->format('D, M d, Y');
+            // $data->etd_wh = Carbon::parse($data->etd_jkt)->subDay(2)->format('D, M d, Y');
+            // $data->eta_destination = Carbon::parse($data->eta_destination)->subDay(2)->format('M d, Y');
+            // $data->etd_jkt = Carbon::parse($data->etd_jkt)->subDay(2)->format('M d, Y');
 
             $actual_container_creation = RegularFixedActualContainerCreation::where('id_fixed_shipping_instruction', $id)->first();
             $actual_container = RegularFixedActualContainer::where('id', $actual_container_creation->id_fixed_actual_container)->get();
