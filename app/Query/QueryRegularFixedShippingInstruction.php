6148a695f420648ffee775850babdd87a5ea1086
alfinorossesa <alfino.zxcvbnm@gmail.com>
2024-08-19 11:14:10 +0700

    fix order no

--- app/Query/QueryRegularFixedShippingInstruction.php
+++ app/Query/QueryRegularFixedShippingInstruction.php
@@ -1344,16 +1344,25 @@ class QueryRegularFixedShippingInstruction extends Model {
             $order_data = [];
             foreach ($data as $item) {
                 $order_data = $item->manyFixedQuantityConfirmation()
-                            ->select('order_no', 'cust_item_no', DB::raw('COUNT(id) as item_count'))  // Aggregate function example
+                            ->select('order_no', 'cust_item_no', DB::raw('COUNT(id) as item_count'))  
                             ->groupBy('order_no', 'cust_item_no')
                             ->orderBy('order_no')
                             ->get()
                             ->toArray();
             }
 
+            $package_number = [];
+            ksort($boxArray);
+            foreach ($boxArray as $order => $value) {
+                foreach ($value as $key => $innerValue) {
+                    $package_number[$order.$key] = count($package_number) + 1;
+                }
+            }
+
             Pdf::loadView('pdf.packaging.packaging_doc',[
                 'data' => $data,
                 'box' => $boxArray,
+                'package_number' => $package_number,
                 'boxYPMJ' => $cek[0]->datasource == "YPMJ" ? QueryRegularFixedQuantityConfirmation::groupByQRCode($box) : [],
                 'gross_weight_per_part' => $gross_weight_per_part,
                 'order_data' => $order_data,
