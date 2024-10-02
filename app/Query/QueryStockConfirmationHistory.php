28648f78ae75e956d6a0b114466d28e52f8b7ac1
alfinorossesa <alfino.zxcvbnm@gmail.com>
2024-10-02 15:04:04 +0700

    update part set

--- app/Query/QueryStockConfirmationHistory.php
+++ app/Query/QueryStockConfirmationHistory.php
@@ -317,7 +317,7 @@ class QueryStockConfirmationHistory extends Model
             if ($item->refRegularDeliveryPlan->item_no == null) {
                 $item_no = [];
                 $item_no_set = $item->refRegularDeliveryPlan->manyDeliveryPlanSet->pluck('item_no')->toArray();
-                $mst = MstBox::whereIn('item_no', $item_no_set)->get();
+                $mst = MstBox::whereIn('item_no', $item_no_set)->orderBy('id')->get();
                 foreach ($mst as $value) {
                     $item_no[] = $value->item_no_series ?? null;
                 }
