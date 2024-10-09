da0a25ca42017be59a7825ad3fd5b81123df87e7
alfinorossesa <alfino.zxcvbnm@gmail.com>
2024-10-09 11:11:04 +0700

    update dok shipping

--- resources/views/pdf/shipping_actual.blade.php
+++ resources/views/pdf/shipping_actual.blade.php
@@ -171,10 +171,6 @@
     
                 </table>
             </table>
-
-            @if (count($hscode) > 2)
-                <div class="page_break"></div>
-            @endif
             
             <table style="border: 1px solid #000; border-top:hidden; border-bottom:hidden;">
                 <table>
@@ -335,6 +331,11 @@
                 </tr>
             </table> --}}
         </table>
+        
+        @if (count($hscode) > 2)
+            <div class="page_break"></div>
+        @endif
+
         <p>Sincerelly Yours</p>
         <table style="width:320px">
             <tr>
