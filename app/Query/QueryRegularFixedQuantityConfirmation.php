a47a29d4af13dd18c481d340a68c32efbbbb318a
alfinorossesa <alfino.zxcvbnm@gmail.com>
2025-01-06 10:21:33 +0700

    update casemark

--- app/Query/QueryRegularFixedQuantityConfirmation.php
+++ app/Query/QueryRegularFixedQuantityConfirmation.php
@@ -1721,7 +1721,7 @@ class QueryRegularFixedQuantityConfirmation extends Model {
                 $count_qty += array_sum($box_item['qty_pcs_box']);
                 $count_net_weight += array_sum($box_item['unit_weight_kg']);
                 $count_gross_weight += array_sum($box_item['total_gross_weight']);
-                $count_meas += (($box_item['length'] * $box_item['width'] * $box_item['height']) / 1000000000);
+                $count_meas += in_array($box_item['length'], ['',null]) ? 0 : (($box_item['length'] * $box_item['width'] * $box_item['height']) / 1000000000);
                 $gross_weight_per_part[] = $box_item['total_gross_weight'];
             }
 
