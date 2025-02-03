7ef05e53b0ef2e0e479032fd4f49b70729ee5a2c
alfinorossesa <alfino.zxcvbnm@gmail.com>
2025-02-03 22:27:23 +0700

    update urutan

--- app/Query/QueryStockConfirmationHistory.php
+++ app/Query/QueryStockConfirmationHistory.php
@@ -652,8 +652,9 @@ class QueryStockConfirmationHistory extends Model
 
                 $regular_delivery_plan_list = RegularDeliveryPlan::where('id_regular_order_entry', $id_regular_order_entry)
                         ->where('bucket_produksi', $bucket_produksi)
-                        ->orderBy('etd_jkt', 'asc')
-                        ->orderBy('case_number', 'asc')
+                        // ->orderBy('etd_jkt', 'asc')
+                        // ->orderBy('case_number', 'asc')
+                        ->orderBy('urutan', 'asc')
                         ->get();
                 
                 $id_regular_delivery_plan_list = [];
